06f1ee1a4b97ea8308f32ee55c2e5fac6f72dc2a
naoki <naokatu4@gmail.com>
2025-04-02 02:53:22 +0900

    feat: error

--- src/main/java/com/example/todoapi/controller/advice/CustomExceptionHandler.java
+++ src/main/java/com/example/todoapi/controller/advice/CustomExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.example.todoapi.controller.advice;
 
+import com.example.todoapi.model.BadRequestError;
 import com.example.todoapi.model.ResourseNotFoundError;
 import com.example.todoapi.service.task.TaskEntityNotFoundException;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -30,6 +32,14 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
             HttpHeaders headers,
             HttpStatusCode status,
             WebRequest request
+    ) {
+        var error = BadRequestErrorCreator.from(ex);
+        return ResponseEntity.badRequest().body(error);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<BadRequestError> handleConstraintViolationException(
+            ConstraintViolationException ex
     ) {
         var error = BadRequestErrorCreator.from(ex);
         return ResponseEntity.badRequest().body(error);
